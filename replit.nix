c392b93ccd59600754014b708d8f5542e469d255
dang do <44604653-huydanginvest@users.noreply.replit.com>
2025-07-04 12:17:48 +0000

    Pre-Recovery checkpoint

---
+++ replit.nix
@@ -0,0 +1,20 @@
+{pkgs}: {
+  deps = [
+    pkgs.haskellPackages.phonetic-languages-phonetics-basics
+    pkgs.gnomeExtensions.window-calls-extended
+    pkgs.ppsspp-qt
+    pkgs.rPackages.motifbreakR
+    pkgs.rubyPackages_3_2.forwardable-extended
+    pkgs.python311Packages.pixel-ring
+    pkgs.haskellPackages.dependent-map_0_2_4_0
+    pkgs.python311Packages.aionanoleaf
+    pkgs.rPackages.neatR
+    pkgs.rPackages.apportion
+    pkgs.gnomeExtensions.net-speed
+    pkgs.cudaPackages.cuda_nvprof
+    pkgs.emacsPackages.solo-jazz-theme
+    pkgs.miniball
+    pkgs.rPackages.DIME
+    pkgs.rPackages.AllPossibleSpellings
+  ];
+}
